f97d35303cf1b3909be0114fbf919b4f8d0f41c8
niranshumittal <22aimit035@ldce.ac.in>
2024-05-28 15:13:24 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
 			<div className="mt-8">
 				<Snippet hideSymbol hideCopyButton variant="flat">
 					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
+						Go for words<Code color="primary">app/page.tsx</Code>
 					</span>
 				</Snippet>
 			</div>
